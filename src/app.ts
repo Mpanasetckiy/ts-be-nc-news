4bdfb6052287935d42f43bed4f31cb3aaede5afb
Maksim Lukianenko <maxbuzz8694@gmail.com>
2024-06-02 00:34:05 +0100

    chore(package.json): add types for swagger-jsdoc and swagger-ui-express to improve type checking and intellisense support feat(package.json): update swagger-jsdoc and swagger-ui-express versions to the latest to ensure compatibility and access to new features feat(schemas.ts): add Swagger documentation for Article, Comment, User, and Topic schemas to improve API documentation and understanding of data structures feat(app.ts): add support for Swagger documentation by including swaggerRouter middleware to serve API documentation feat(swagger.ts): create swaggerRouter to generate and serve Swagger documentation for the API

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import express, { Application } from "express";
 import cors from "cors";
 
 import routes from "./api/routes";
+import swaggerRouter from "./swagger";
 
 import { pgErrorHandling, errorHandling } from "./middleware/error-handling";
 
 const app: Application = express();
+
+// * Swagger
+app.use(swaggerRouter);
 
 // * Tech endpoints
 app.use(cors());
---
+++ src/swagger.ts
@@ -0,0 +1,30 @@
+import swaggerUi from "swagger-ui-express";
+import { Router } from "express";
+import swaggerJSDoc from "swagger-jsdoc";
+
+const router = Router();
+
+// Swagger configuration
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "NC NEWS API Documentation",
+      version: "1.0.0",
+      description: "API documentation generated with Swagger",
+    },
+    servers: [
+      {
+        url: "http://localhost:8000",
+        description: "Development server",
+      },
+    ],
+  },
+  apis: ["./src/api/controllers/**/*.ts"], // Path to the API routes/controllers
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+export default router;
