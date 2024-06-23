58f7f686c9d739b03db2836a1ffda86ba5ac949b
Maksim Lukianenko <maxbuzz8694@gmail.com>
2024-06-23 21:54:38 +0100

    fix(connection.ts): refactor connection configuration to support Supabase as a database provider in production environment feat(connection.ts): add support for environment variables PG_USER and PG_PASSWORD to connect to PostgreSQL database

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -12,15 +12,22 @@ if (!process.env.PG_DATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-const config: any = { dialect: "postgres", logging: false, host: "localhost" };
+const config: any = {
+  dialect: "postgres",
+  logging: false,
+  host: "localhost",
+};
 
-if (ENV === "production") {
-  config.url = process.env.DATABASE_URL;
-  config.pool = {
-    max: 2,
-  };
-}
+const supabaseString = process.env.DATABASE_URL;
 
-const db = new Sequelize(process.env.PG_DATABASE, "maxbmaapc", "", config);
+const db =
+  ENV === "production"
+    ? new Sequelize(supabaseString)
+    : new Sequelize(
+        process.env.PG_DATABASE,
+        process.env.PG_USER,
+        process.env.PG_PASSWORD,
+        config
+      );
 
 export default db;
